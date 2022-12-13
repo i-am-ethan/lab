b213caed0a45651ad8d558634d88e39766f730bb
ethan <izawaryosuke0101@gmail.com>
2022-12-14 02:55:03 +0900

    props

--- react-zissen-kyokasyo/my-app/src/components/ColorMessage.jsx
+++ react-zissen-kyokasyo/my-app/src/components/ColorMessage.jsx
@@ -1,10 +1,13 @@
 export const ColorMessage = (props) => {
   console.log(props)
+  // propsを分割代入する
+  const {color, children} = props;
   const contentStyle = {
     // color: "blue",
-    color: props.color,
+    // color: color,
+    color,
     fontSize: "20px",
   }
 
-  return <p style={contentStyle}>{props.children}</p>
+  return <p style={contentStyle}>{children}</p>
 }
