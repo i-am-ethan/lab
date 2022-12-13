bc8ed26c47d5d6147a3588c51a9647d7cf81a65c
ethan <izawaryosuke0101@gmail.com>
2022-12-14 02:28:19 +0900

    スタイルの適用2

--- react-zissen-kyokasyo/my-app/src/App.jsx
+++ react-zissen-kyokasyo/my-app/src/App.jsx
@@ -2,10 +2,19 @@ export const App = ()=>{
   const onClickButton = () => {
     alert()
   }
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px",
+  }
+  const contentPinkStyle = {
+    color: "pink",
+    fontSize: "20px",
+  }
   return(
   <>
-    <h1 style={{color:"red"}}>こんにちは!</h1>
+    <h1 style={contentStyle}>こんにちは!</h1>
     <p>こんばんは</p>
+    <p style={contentPinkStyle}>元気です！</p>
     <button onClick={onClickButton}>ボタン</button>
   </>
   )
