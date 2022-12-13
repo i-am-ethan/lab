0a3b096b830a131e464fe8c1914017d78e5e2d7a
ethan <izawaryosuke0101@gmail.com>
2022-12-14 03:22:29 +0900

    不要なコードを削除

--- react-zissen-kyokasyo/my-app/src/App.jsx
+++ react-zissen-kyokasyo/my-app/src/App.jsx
@@ -5,18 +5,14 @@ export const App = ()=>{
   const [num, setNum] = useState(0)
 
   const onClickButton = () => {
-    // alert()
     setNum(num+1)
   }
   return(
   <>
     <h1 style={{color:"red"}}>こんにちは!</h1>
     <p>こんばんは</p>
-    {/* <ColorMessage color="blue" message="お元気ですか？" /> */}
     <ColorMessage color="blue">お元気ですか？</ColorMessage>
-    {/* <ColorMessage color="pink" message="元気です！" /> */}
     <ColorMessage color="pink">元気です！</ColorMessage>
-    {/* <p style={contentPinkStyle}>元気です！</p> */}
     <button onClick={onClickButton}>ボタン</button>
     <p>{num}</p>
   </>
