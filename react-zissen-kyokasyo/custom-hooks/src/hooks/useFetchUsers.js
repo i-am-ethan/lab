b8fdb253d3b9f84e28bcebc68c10ad08f96708e6
ethan <izawaryosuke0101@gmail.com>
2022-12-31 22:06:04 +0900

    カスタムフック実装

---
+++ react-zissen-kyokasyo/custom-hooks/src/hooks/useFetchUsers.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import axios from "axios"
+
+export const userFetchUsers = () => {
+  // const [userList, setUserList] = useState([{id : 1}])
+
+  const [userList, setUserList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  // const onClickFetchUser = () => alert('関数実行')
+
+  const onClickFetchUser = () => {
+    setIsLoading(true)
+    setIsError(false)
+
+    axios
+    .get('https://example.com/users')
+    .then(result => {
+      const users = result.data.map(user=>({
+        id:user.id,
+        name:`${user.lastname} ${user.firstname}`,
+        age:user.age,
+      }))
+      setUserList(users);
+    })
+    .catch(()=>{
+      setIsError(true)
+    })
+    .finally(()=>{
+      setIsLoading(false)
+    })
+  }
+
+  return { userList,isLoading, isError, onClickFetchUser}
+}
