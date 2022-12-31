6a0aa3c99434173d59ae81857d34a29a48d40d1a
ethan <izawaryosuke0101@gmail.com>
2022-12-31 15:30:35 +0900

    propsで値を受け渡す

--- react-zissen-kyokasyo/my-app/src/App.jsx
+++ react-zissen-kyokasyo/my-app/src/App.jsx
@@ -1,15 +1,14 @@
+import { ColorMessage } from "./components/ColorMessage";
+
 export const App = () => {
   const onClickButton = () => {
    alert() ;
   }
-  const contentStyle = {
-    color: "blue",
-    fontSize: "20px",
-  }
   return (
     <>
     <h1 style={{color:"red"}}>こんにちは！</h1>
-    <p style={contentStyle}>お元気ですか？</p>
+    <ColorMessage color="blue" message="お元気ですか?"/>
+    <ColorMessage color="pink" message="元気です！"/>
     <button onClick={onClickButton}>ボタン</button>
     </>
   )
---
+++ react-zissen-kyokasyo/my-app/src/components/ColorMessage.jsx
@@ -0,0 +1,8 @@
+export const ColorMessage = (props) => {
+  console.log(props)
+  const contentStyle = {
+    color: props.color,
+    fontSize: "20px",
+  }
+  return <p style={contentStyle}>{props.message}</p>
+}
