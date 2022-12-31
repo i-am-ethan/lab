d71c1cb41f8a2c25782015ec7fe461531bbed6a0
ethan <izawaryosuke0101@gmail.com>
2022-12-31 21:29:29 +0900

    typescript

--- react-zissen-kyokasyo/my-ts-app/src/index.tsx
+++ react-zissen-kyokasyo/my-ts-app/src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from "react-dom/client"
+import {App} from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root')
+if (!container) throw new Error('Failed to find the root element');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(container)
+root.render(<App />)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { App } from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
