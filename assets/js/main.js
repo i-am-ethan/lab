3bc8760d0bf0060434f3753892fc9dafd7b1c5d7
ethan <izawaryosuke0101@gmail.com>
2023-01-03 23:08:08 +0900

    fetchで取得した値を関数の引数に代入する

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,12 @@
 import {createArticleEle} from "./components/createArticle.js"
 
-console.log('main.js')
-
-
 // get json file
 fetch('../assets/json/articles.json')
 .then((res)=>{
   return res.json()
 })
 .then((json)=>{
-  console.log(json)
+  for(const article of json){
+    createArticleEle(article.href, article.src, article.title_alt, article.desc)
+  }
 })
-
-createArticleEle(
-  "./20221028_webgl-img-slider/index.html",
-  "./assets/img/thumbnail/webgl-image-transition.jpg",
-  "WebGL Image Transition",
-  "WebGLを用いた画像切り替え",
-)
-createArticleEle(
-  "./20221028_webgl-img-slider/index.html",
-  "./assets/img/thumbnail/webgl-image-transition.jpg",
-  "WebGL Image Transition",
-  "WebGLを用いた画像切り替え",
-)
