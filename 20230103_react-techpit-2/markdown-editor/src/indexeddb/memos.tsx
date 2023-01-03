16ecb1cb2dc69d6fe378696065783fdeb29fdced
ethan <izawaryosuke0101@gmail.com>
2023-01-03 21:00:47 +0900

    不要なコメントアウト削除

--- 20230103_react-techpit-2/markdown-editor/src/indexeddb/memos.tsx
+++ 20230103_react-techpit-2/markdown-editor/src/indexeddb/memos.tsx
@@ -15,21 +15,3 @@ export const putMemo = async (title: string, text: string): Promise<void> => {
   await memos.put({ datetime, title, text })
 }
 
-
-// import Dexie from "dexie";
-
-// // IndexedDBに保存するデータの型定義
-// export interface MemoRecord{
-//   datetime:string
-//   title:string
-//   text:string
-// }
-
-// const database = new Dexie('markdown-editor') // Dexieインスタンス生成
-// database.version(1).stores({memos: '&datetime'})
-// const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
-
-// export const putMemo = async(title:string, text:string): Promise<void> => {
-//   const datetime = new Date().toISOString()
-//   await memos.put({datetime, title, text})
-// }
