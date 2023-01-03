b58106bdec2a3375f24aa08eafdfef189dd113f3
ethan <izawaryosuke0101@gmail.com>
2023-01-03 19:09:45 +0900

    モーダルとボタンを追加

--- 20230103_react-techpit-2/markdown-editor/src/indexeddb/memos.tsx
+++ 20230103_react-techpit-2/markdown-editor/src/indexeddb/memos.tsx
@@ -1,17 +1,35 @@
-import Dexie from "dexie";
+import Dexie from 'dexie'
 
-// IndexedDBに保存するデータの型定義
-export interface MemoRecord{
-  datetime:string
-  title:string
-  text:string
+export interface MemoRecord {
+  datetime: string
+  title: string
+  text: string
 }
 
-const database = new Dexie('markdown-editor') // Dexieインスタンス生成
-database.version(1).stores({memos: '&datatime'})
+const database = new Dexie('markdown-editor')
+database.version(1).stores({ memos: '&datetime' })
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
 
-export const putMemo = async(title:string, text:string): Promise<void> => {
+export const putMemo = async (title: string, text: string): Promise<void> => {
   const datetime = new Date().toISOString()
-  await memos.put({datetime, title, text})
+  await memos.put({ datetime, title, text })
 }
+
+
+// import Dexie from "dexie";
+
+// // IndexedDBに保存するデータの型定義
+// export interface MemoRecord{
+//   datetime:string
+//   title:string
+//   text:string
+// }
+
+// const database = new Dexie('markdown-editor') // Dexieインスタンス生成
+// database.version(1).stores({memos: '&datetime'})
+// const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
+
+// export const putMemo = async(title:string, text:string): Promise<void> => {
+//   const datetime = new Date().toISOString()
+//   await memos.put({datetime, title, text})
+// }
