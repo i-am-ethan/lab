9d90b9191690dc95dc601c242e57494b99b70724
ethan <izawaryosuke0101@gmail.com>
2023-01-06 16:35:55 +0900

    コメント追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,14 @@ import {createArticleEle} from "./components/createArticle.js"
 // get json file
 fetch('../assets/json/articles.json')
 .then((res)=>{
+  console.log(res)
   return res.json()
 })
 .then((json)=>{
   for(const article of json){
     createArticleEle(article.href, article.src, article.title_alt, article.desc)
   }
+})
+.catch((error)=>{
+  console.log(error)
 })
